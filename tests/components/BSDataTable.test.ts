c3cb57804f3e14375f300817cd2aa55726c778d0
mvcguy <me.shahidali@hotmail.com>
2022-05-08 23:02:30 +0200

    unit tests

--- tests/components/BSDataTable.test.ts
+++ tests/components/BSDataTable.test.ts
@@ -1,3 +1,4 @@
+import { BSDataTableInput } from "src";
 import { BSDataTablePagingMetaData } from "../../src/commonTypes/common-types";
 import { BSFluentBuilder } from "../../src/components/BSFluentBuilder";
 
@@ -22,9 +23,9 @@ function getTable() {
         })
         .AddInitData((data) => {
             data.initData = [
-                { first_name: 'Shahid', last_name: "Khan", address: 'Home' },
-                { first_name: 'Wajid', last_name: "Khan", address: 'Home' },
-                { first_name: 'Asad', last_name: "Khan", address: 'Home' }
+                { first_name: 'Shahid', last_name: "Khan", address: 'Home1' },
+                { first_name: 'Wajid', last_name: "Khans", address: 'Home2' },
+                { first_name: 'Asad', last_name: "Khanz", address: 'Home3' }
             ];
             data.metaData = new BSDataTablePagingMetaData(1, 10, 3);
         })
@@ -115,35 +116,78 @@ describe('BSDataTable', function () {
             var cells = row.cells;
             expect(cells.length).toBe(4);
             cells.forEach((cell, ci) => {
+                expect(cell.element.tagName).toBe('TD');
                 // cell at zero index is the row marker
                 if (ci === 0) {
-                    var rowMarker = cell;
-                    
+                    var rowMarker = cell.children[0];
+                    expect(rowMarker.hasClasses('bi bi-caret-right row-marker')).toBe(true);
+                    expect(rowMarker.element.tagName).toBe('I');
                 }
                 else {
-
+                    var rowInput = cell.children[0] as BSDataTableInput;
+                    expect(rowInput.hasClasses('form-control form-control-sm')).toBe(true);
+                    // expect(rowInput.id).toBe(`${rowInput.options.InputType}_${rowInput.options.DataSourceName}_${rowInput.options.ModelName}_${index + 1}`);
+                    expect(rowInput.title).toBe(rowInput.options.Title);
+                    expect(rowInput.element['placeholder']).toBe(rowInput.options.PlaceHolder);
+                    expect(rowInput.element.tagName).toBe('INPUT');
+                    expect(rowInput.element['type']).toBe('text');
                 }
             })
 
         });
-
-        // console.log(dt.element.parentElement.innerHTML);
-
-
     })
 
     it('verifies input controls of the component', function () {
-        // document.body.innerHTML = '<div id="container2"></div>';
-        // var dt = getTable()
-        //     .Build()
-        //     .RegisterCallbacks()
-        //     .Render();
+        document.body.innerHTML = '<div id="container2"></div>';
+        var dt = getTable()
+            .Build()
+            .RegisterCallbacks()
+            .Render();
 
-        // var records = dt.allRecords;
-        // expect(records.length).toBe(3);
-        // // console.log(records);
-        // //var row = dt.getRowByIndex(2); // position 1 is template row
-        // // console.log(row.getRowData());
+        var records = dt.allRecords;
+
+        expect(records.length).toBe(3);
+        expect(dt.body.rows.length).toBe(4);
+
+        dt.body.rows.forEach((row, index) => {
+
+            var rowExt = row.getRowDataExt();
+            var record = records[Math.max(0, index - 1)]; // records are 3, while rows are 4 (one row is template row)
+            switch (index) {
+                case 0:
+                    // template row
+                    break;
+                case 1:
+                    expect(rowExt.first_name.val).toBe('Shahid');
+                    expect(rowExt.last_name.val).toBe('Khan');
+                    expect(rowExt.address.val).toBe('Home1');
+
+                    expect(record.first_name).toBe('Shahid');
+                    expect(record.last_name).toBe('Khan');
+                    expect(record.address).toBe('Home1');
+
+                    break;
+                case 2:
+                    expect(rowExt.first_name.val).toBe('Wajid');
+                    expect(rowExt.last_name.val).toBe('Khans');
+                    expect(rowExt.address.val).toBe('Home2');
+
+                    expect(record.first_name).toBe('Wajid');
+                    expect(record.last_name).toBe('Khans');
+                    expect(record.address).toBe('Home2');
+
+                    break;
+                case 3:
+                    expect(rowExt.first_name.val).toBe('Asad');
+                    expect(rowExt.last_name.val).toBe('Khanz');
+                    expect(rowExt.address.val).toBe('Home3');
+
+                    expect(record.first_name).toBe('Asad');
+                    expect(record.last_name).toBe('Khanz');
+                    expect(record.address).toBe('Home3');
+                    break;
+            }
+        });
 
         // var row = dt.body.rows.find(x => x.getRowDataExt().first_name.val === "Shahid");
         // var shaRow = row.getRowDataExt();
@@ -154,10 +198,9 @@ describe('BSDataTable', function () {
         // dt.body.focusRow(row);
         // dt.body.markDeleted();
 
-        // // var newRow = dt.addNewRow({ first_name: 'Safia', last_name: 'Khan', address: 'office' }, false);        
-        // // dt.body.addRow(newRow);
+        // var newRow = dt.addNewRow({ first_name: 'Safia', last_name: 'Khan', address: 'office' }, false);        
+        // dt.body.addRow(newRow);
         // var newRow = dt.addEmptyRow();
-
         // var rowExt = newRow.getRowDataExt();
         // rowExt.first_name.val = 'Moniba';
         // rowExt.last_name.val = 'Khan';
